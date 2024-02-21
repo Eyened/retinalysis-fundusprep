be66aaf1b123d449d5087096af4591f17f52422b
Jose <j.vargasquiros@erasmusmc.nl>
2024-02-21 15:57:17 +0000

    auto-formatted code and added vscode settings

--- rtnls_fundusprep/colors.py
+++ rtnls_fundusprep/colors.py
@@ -1,7 +1,7 @@
-import numpy as np
 import cv2
-from skimage.filters import sato
+import numpy as np
 from scipy.ndimage import gaussian_filter
+from skimage.filters import sato
 
 
 def rgb_to_luminance(image):
@@ -9,14 +9,14 @@ def rgb_to_luminance(image):
 
 
 def convert_to_gray(self, image, method):
-    if method == 'luminance':
+    if method == "luminance":
         return rgb_to_luminance(image)
-    channel_index = {'red': 0, 'green': 1, 'blue': 2}[method]
+    channel_index = {"red": 0, "green": 1, "blue": 2}[method]
     return image[:, :, channel_index]
 
 
 def gamma_correction(image, gamma):
-    return image ** gamma
+    return image**gamma
 
 
 def to_uint8(image):
@@ -41,13 +41,11 @@ def contrast_enhance(image, mask, sigma, contrast_factor=4):
 
 
 def CLAHE(image, cliplimit=2, tilesize=8):
-
     rgb = to_uint8(image)
 
     lab = cv2.cvtColor(rgb, cv2.COLOR_RGB2LAB)
 
-    clahe = cv2.createCLAHE(clipLimit=cliplimit,
-                            tileGridSize=(tilesize, tilesize))
+    clahe = cv2.createCLAHE(clipLimit=cliplimit, tileGridSize=(tilesize, tilesize))
     lab[:, :, 0] = clahe.apply(lab[:, :, 0])
 
     image_clahe = cv2.cvtColor(lab, cv2.COLOR_LAB2RGB)
@@ -58,20 +56,20 @@ def combine_red_blue(img0, img1):
     if len(img0.shape) == 3:
         img0_gray = rgb_to_luminance(img0)
     else:
-        img0_gray = img0 
-    
+        img0_gray = img0
+
     if len(img1.shape) == 3:
         img1_gray = rgb_to_luminance(img1)
     else:
         img1_gray = img1
-    
+
     img0 /= img0.max()
     img1 /= img1.max()
-    
+
     h, w = img0_gray.shape
     combined_img = np.zeros((h, w, 3))
     combined_img[:, :, 0] = img1_gray
     combined_img[:, :, 1] = img0_gray
     combined_img[:, :, 2] = img0_gray
-    
+
     return combined_img
