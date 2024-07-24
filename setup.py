508e49ac3b87e27ee4e44092668570a94bad8da0
Jose <j.vargasquiros@erasmusmc.nl>
2024-07-24 21:39:03 +0200

    adding reqs

--- setup.py
+++ setup.py
@@ -7,6 +7,15 @@ setup(
     author_email="your.email@example.com",
     description="A brief description of package1",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=[
+        "numpy == 1.*",
+        "pandas == 2.*",
+        "scikit-learn==1.*",
+        "opencv-python == 4.*",
+        "matplotlib==3.*",
+        "joblib==1.*",
+        "Pillow == 9.*",
+        "click==8.*",
+    ],
     package_data={},
 )
