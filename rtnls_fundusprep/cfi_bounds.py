remove masking contrast enhanced, fix scale when flipped transform

rtnls_fundusprep/cfi_bounds.py
@@ -84,7 +84,6 @@ class CFIBounds:
         blurred = T.warp_inverse(blurred_warped, (self.h, self.w))
         ce = unsharp_masking(self.image / 255, blurred,
                              contrast_factor, sharpen)
-        ce[~self.mask] = 0
         return to_uint8(ce)
 
     def make_binary_mask(self, shrink_ratio=0.01):
@@ -166,7 +165,6 @@ class CFIBounds:
         image = self.mirrored_image / 255
         blurred = gaussian_filter(image, sigma=(sigma, sigma, 0))
         ce = unsharp_masking(image, blurred, contrast_factor)
-        ce[~self.mask] = 0
         return to_uint8(ce)
 
     def get_cropping_transform(self, target_diameter, patch_size=None):
@@ -219,7 +217,7 @@ class CFIBounds:
         return {
             'center': (self.cx, self.cy),
             'radius': self.radius,
-            'lines': {k: (v.tolist() if isinstance(v, np.ndarray) else v) for k, v in self.lines.items()}
+            'lines': {k: (v.tolist() if isinstance(v, np.ndarray) else list(v)) for k, v in self.lines.items()},
         }
 
     @classmethod

rtnls_fundusprep/transformation.py
@@ -28,8 +28,8 @@ class ProjectiveTransform:
     def scale(self):
         # self.M is a 3x3 matrix
         # return the scaling factor as a single scalar
-        return np.sqrt(np.linalg.det(self.M[:2, :2]))
-
+        return np.sqrt(np.abs(np.linalg.det(self.M[:2, :2])))
+    
     def apply(self, points):
         # Add homogeneous coordinate (1) to each point
         points_homogeneous = np.column_stack([points, np.ones(len(points))])
@@ -51,7 +51,7 @@ class ProjectiveTransform:
             return np.ceil(self.apply(corners).max(axis=0)).astype(int)
         else:
             h, w = out_size
-            return w, h
+            return int(np.ceil(w)), int(np.ceil(h))
 
     def _apply_warp(self, image, out_size, M, mode):
         dsize = self.get_dsize(image, out_size)